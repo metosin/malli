79be7e8be4b718ee4cc5cfce36a07f89f1db11cd
Tommi Reiman <tommi@iki.fi>
2021-07-14 00:11:49 +0300

    more docs

--- src/malli/instrument.clj
+++ src/malli/instrument.clj
@@ -4,7 +4,6 @@
 
 (defn -find-var [n s] (find-var (symbol (str n "/" s))))
 
-(defn -filter-all [] (constantly true))
 (defn -filter-ns [& ns] (fn [n _ _] ((set ns) n)))
 (defn -filter-var [f] (fn [n s _] (f (-find-var n s))))
 (defn -filter-schema [f] (fn [_ _ {:keys [schema]}] (f schema)))
@@ -34,6 +33,15 @@
 ;;
 
 (defn collect!
+  "Reads all public Vars from a given namespace and registers a function (var) schema if `:malli/schema`
+   metadata is present. The following metadata key can be used:
+
+   | key             | description |
+   | ----------------|-------------|
+   | `:malli/schema` | function schema
+   | `:malli/scope`  | optional set of scope definitions, defaults to `#{:input :output}`
+   | `:malli/report` | optional side-effecting function of `key data -> any` to report problems, defaults to `m/-fail!`
+   | `:malli/gen`    | optional value `true` or function of `schema -> schema -> value` to be invoked on the args to get the return value"
   ([] (collect! {:ns *ns*}))
   ([{:keys [ns]}]
    (not-empty
@@ -44,20 +52,29 @@
            (cond-> acc v' (conj v)))) #{} (vals (ns-publics ns))))))
 
 (defn instrument!
+  "Applies instrumentation for a filtered set of function Vars (e.g. `defn`s).
+   See [[malli.core/-instrument]] for possible options."
   ([] (instrument! nil))
   ([options] (-strument! (assoc options :mode :instrument))))
 
 (defn unstrument!
+  "Removes instrumentation from a filtered set of function Vars (e.g. `defn`s).
+   See [[malli.core/-instrument]] for possible options."
   ([] (unstrument! nil))
   ([options] (-strument! (assoc options :mode :unstrument))))
 
-(defn stop! []
+(defn stop!
+  "Stops instrumentation for all functions vars and removes clj-kondo type annotations."
+  []
   (remove-watch @#'m/-function-schemas* ::watch)
   (unstrument!)
   (clj-kondo/emit!)
   (println "stopped instrumentation"))
 
 (defn start!
+  "Starts instrumentation for a filtered set of function Vars (e.g. `defn`s).
+   See [[malli.core/-instrument]] for possible options. Re-instruments if the
+   function schemas change. Also emits clj-kondo type annotations."
   ([] (start! nil))
   ([options]
    (with-out-str (stop!))

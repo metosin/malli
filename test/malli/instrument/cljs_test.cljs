9a72f0442b3ff48e8454698eb7d03d83c0dd902d
Tommi Reiman <tommi@iki.fi>
2022-01-08 21:12:40 +0200

    cljs instrumentation tests for dx/defn

--- test/malli/instrument/cljs_test.cljs
+++ test/malli/instrument/cljs_test.cljs
@@ -2,6 +2,7 @@
   (:require
     [cljs.test :refer [deftest is testing]]
     [malli.core :as m]
+    [malli.experimental :as mx]
     [malli.instrument.cljs :as mi]))
 
 (defn plus [x] (inc x))
@@ -17,29 +18,44 @@
 
 (defn ->minus [] minus)
 
+(mx/defn power :- [:int {:max 6}]
+  "inlined schema power"
+  [x :- :int] (* x x))
+
+(defn ->power [] power)
+
 (deftest instrument!-test
 
   (testing "without instrumentation"
-    (with-out-str (mi/unstrument! {:filters [(mi/-filter-ns 'malli.instrument.cljs-test)]}))
+    (mi/unstrument! {:filters [(mi/-filter-ns 'malli.instrument.cljs-test)]})
+
     (is (= "21" ((->plus) "2")))
-    (is (= 7 ((->plus) 6))))
+    (is (= 7 ((->plus) 6)))
+
+    (is (= 4 ((->power) "2")))
+    (is (= 36 ((->power) 6))))
 
   (testing "with instrumentation"
-    (with-out-str (mi/instrument! {:filters [(mi/-filter-ns 'malli.instrument.cljs-test)]}))
+    (mi/instrument! {:filters [(mi/-filter-ns 'malli.instrument.cljs-test)]})
+
     (is (thrown-with-msg? js/Error #":malli.core/invalid-input" ((->plus) "2")))
-    (is (thrown-with-msg? js/Error #":malli.core/invalid-output" ((->plus) 6)))))
+    (is (thrown-with-msg? js/Error #":malli.core/invalid-output" ((->plus) 6)))
+
+    (is (thrown-with-msg? js/Error #":malli.core/invalid-input" ((->power) "2"))
+    (is (thrown-with-msg? js/Error #":malli.core/invalid-output" ((->power) 6))))))
 
 (mi/collect!)
 
 (deftest collect!-test
 
   (testing "without instrumentation"
+    (mi/unstrument! {:filters [(mi/-filter-ns 'malli.instrument.cljs-test)]})
 
-    (with-out-str (mi/unstrument! {:filters [(mi/-filter-ns 'malli.instrument.cljs-test)]}))
     (is (= 1 ((->minus) "2")))
     (is (= 5 ((->minus) 6))))
 
   (testing "with instrumentation"
-    (with-out-str (mi/instrument! {:filters [(mi/-filter-ns 'malli.instrument.cljs-test)]}))
+    (mi/instrument! {:filters [(mi/-filter-ns 'malli.instrument.cljs-test)]})
+
     (is (thrown-with-msg? js/Error #":malli.core/invalid-input" ((->minus) "2")))
     (is (thrown-with-msg? js/Error #":malli.core/invalid-output" ((->minus) 6)))))
